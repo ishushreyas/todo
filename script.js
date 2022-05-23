e174df8e242aeddf0ee8ce1997c9d07aa28c07df
Ishu Shreyas <98636163+ishushreyas@users.noreply.github.com>
2022-05-23 20:07:31 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+const firebaseConfig = {
+
+  apiKey: "AIzaSyDnRiLq8uIV3vMmc-e5oPpuOYW9qth9ZjU",
+
+  authDomain: "todo-live-8ee63.firebaseapp.com",
+
+  projectId: "todo-live-8ee63",
+
+  storageBucket: "todo-live-8ee63.appspot.com",
+
+  messagingSenderId: "341389748397",
+
+  appId: "1:341389748397:web:1110e11505ad49078a7e24",
+
+  measurementId: "G-63LMB8M8XN"
+
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+const analytics = getAnalytics(app);
